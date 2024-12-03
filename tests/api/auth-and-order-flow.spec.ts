b01e8847dc7d7e9f429a619f4416152e08cf8c43
AngpeuK <angpeu@gmail.com>
2024-12-04 00:34:45 +0200

    Update auth-and-order-flow.spec.ts

--- tests/api/auth-and-order-flow.spec.ts
+++ tests/api/auth-and-order-flow.spec.ts
@@ -8,28 +8,27 @@ const loginPath = 'login/student'
 const orderPath = 'orders'
 
 test.describe('Tallinn delivery API tests', () => {
-  test('login and create order', async ({ request }) => {
+  test('1 login and create order', async ({ request }) => {
     const requestBody = LoginDto.createLoginWithCorrectData()
-
     const loginResponse = await request.post(`${serviceURL}${loginPath}`, {
       data: requestBody,
     })
+    console.log('1 Logged in')
 
     const jwt = await loginResponse.text()
     const orderData = OrderDto.createOrderWithRandomData()
-
     const orderResponse = await request.post(`${serviceURL}${orderPath}`, {
       data: orderData,
       headers: {
         Authorization: `Bearer ${jwt}`,
       },
     })
-    console.log('Generated Order Data:', orderData)
+    console.log('1 Generated Order from DTO:', orderData)
 
     const orderResponseBody = await orderResponse.json()
-    await new Promise((resolve) => setTimeout(resolve, 3000))
-    console.log('orderResponse status:', orderResponse.status())
-    console.log('orderResponseBody:', orderResponseBody)
+    console.log('1 Received PostOrderResponse status:', orderResponse.status())
+    console.log('1 Received PostOrderResponseBody:', orderResponseBody)
+
     if (
       orderResponse.status() !== StatusCodes.INTERNAL_SERVER_ERROR &&
       orderResponse.status() !== StatusCodes.BAD_REQUEST
@@ -39,28 +38,30 @@ test.describe('Tallinn delivery API tests', () => {
       expect.soft(orderResponseBody.id).toBeDefined()
     }
   })
-  test('login and get order', async ({ request }) => {
+
+
+
+  test('2 login create and get order', async ({ request }) => {
     const requestBody = LoginDto.createLoginWithCorrectData()
     const loginResponse = await request.post(`${serviceURL}${loginPath}`, {
       data: requestBody,
     })
+    console.log('2 Logged in')
 
     const jwt = await loginResponse.text()
     const orderData = OrderDto.createOrderWithRandomData()
-
-    console.log('Generated Order Data:', orderData)
-
     const orderResponse = await request.post(`${serviceURL}${orderPath}`, {
       data: orderData,
       headers: {
         Authorization: `Bearer ${jwt}`,
       },
     })
+    console.log('2 Generated Order from DTO:', orderData)
 
     const orderResponseBody = await orderResponse.json()
-    await new Promise((resolve) => setTimeout(resolve, 3000))
-    console.log('orderResponse status:', orderResponse.status())
-    console.log('POSTorderResponseBody:', orderResponseBody)
+    console.log('2 Received PostOrderResponse status:', orderResponse.status())
+    console.log('2 Received PostOrderResponseBody:', orderResponseBody)
+
     if (
       orderResponse.status() !== StatusCodes.INTERNAL_SERVER_ERROR &&
       orderResponse.status() !== StatusCodes.BAD_REQUEST
@@ -72,31 +73,36 @@ test.describe('Tallinn delivery API tests', () => {
 
     const orderId = orderResponseBody.id
     const getResponse = await request.get(`${serviceURL}${orderPath}/${orderId}`, {
-      headers: { Authorization: `Bearer ${jwt}` },
+      headers: {
+        Authorization: `Bearer ${jwt}`
+      },
     })
 
     const getResponseBody = await getResponse.json()
-    await new Promise((resolve) => setTimeout(resolve, 3000))
-    console.log('orderResponse status:', getResponse.status())
-    console.log('GETorderResponseBody:', getResponseBody)
+    console.log('2 Received GetOrderResponse status:', getResponse.status())
+    console.log('2 Received GetOrderResponseBody:', getResponseBody)
+
     if (
       getResponse.status() !== StatusCodes.INTERNAL_SERVER_ERROR &&
       getResponse.status() !== StatusCodes.BAD_REQUEST
     ) {
+
       expect.soft(getResponse.status()).toBe(StatusCodes.OK)
       expect.soft(getResponseBody.status).toBe('OPEN')
       expect.soft(getResponseBody.id).toBe(orderId)
     }
   })
-  test('login and delete order', async ({ request }) => {
+
+  test('3 login create and delete order', async ({ request }) => {
     const requestBody = LoginDto.createLoginWithCorrectData()
     const loginResponse = await request.post(`${serviceURL}${loginPath}`, {
       data: requestBody,
     })
+    console.log('3 Logged in')
 
     const jwt = await loginResponse.text()
     const orderData = OrderDto.createOrderWithRandomData()
-    console.log('Generated Order Data:', orderData)
+    console.log('3 Generated Order from DTO:', orderData)
 
     const orderResponse = await request.post(`${serviceURL}${orderPath}`, {
       data: orderData,
@@ -106,9 +112,10 @@ test.describe('Tallinn delivery API tests', () => {
     })
 
     const orderResponseBody = await orderResponse.json()
-    await new Promise((resolve) => setTimeout(resolve, 3000))
-    console.log('orderResponse status:', orderResponse.status())
-    console.log('POSTorderResponseBody:', orderResponseBody)
+    console.log('3 Received PostOrderResponse status:', orderResponse.status())
+    console.log('3 Received PostOrderResponseBody:', orderResponseBody)
+    console.log('3 Received PostOrderResponseText:', await orderResponse.text())
+
     if (
       orderResponse.status() !== StatusCodes.INTERNAL_SERVER_ERROR &&
       orderResponse.status() !== StatusCodes.BAD_REQUEST
@@ -119,17 +126,22 @@ test.describe('Tallinn delivery API tests', () => {
     }
 
     const orderId = orderResponseBody.id
-    const getResponse = await request.delete(`${serviceURL}${orderPath}/${orderId}`, {
-      headers: { Authorization: `Bearer ${jwt}` },
+    const deleteResponse = await request.delete(`${serviceURL}${orderPath}/${orderId}`, {
+      headers: {
+        Authorization: `Bearer ${jwt}`
+      },
+
     })
-    await new Promise((resolve) => setTimeout(resolve, 3000))
-    console.log('orderResponse status:', orderResponse.status())
-    console.log('GETorderResponseBody:', orderResponseBody)
+
+    console.log('3 Received DeleteOrderResponse status:', deleteResponse.status())
+    console.log('3 Received DeleteOrderResponseText:', await deleteResponse.text())
+
     if (
-      getResponse.status() !== StatusCodes.INTERNAL_SERVER_ERROR &&
-      getResponse.status() !== StatusCodes.BAD_REQUEST
+      deleteResponse.status() !== StatusCodes.INTERNAL_SERVER_ERROR &&
+      deleteResponse.status() !== StatusCodes.BAD_REQUEST
     ) {
-      expect.soft(getResponse.status()).toBe(StatusCodes.OK)
+
+      expect.soft(deleteResponse.status()).toBe(StatusCodes.OK)
     }
   })
 })
